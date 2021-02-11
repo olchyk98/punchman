38e45fc8b827f0b0e432f7f38c46c2fb270ed29b
DaTweaks <51744514+DaTweaks@users.noreply.github.com>
2021-02-11 14:21:14 +0100

    chore(character-select)
    made the scene look better lmao

---
+++ Assets/Scripts/UI/ArrowSelect.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ArrowSelect : MonoBehaviour
+{
+    private Image image;
+
+    public List<Sprite> Sprites = new List<Sprite>();
+    // Start is called before the first frame update
+    void Awake()
+    {
+        image = GetComponent<Image>();
+    }
+    
+    public void ArrowChange(int index)
+    {
+        image.sprite = Sprites[index];
+    }
+}
