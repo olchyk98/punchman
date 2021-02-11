824a9f2924956317a7d794a8329b3390134d55c7
Tristan <tristan@tristanfarkas.dev>
2021-02-11 11:28:19 +0100

    refactor(folders): Changes gameplay folder structure

---
+++ Assets/Scripts/Gameplay/Powerups/IPowerup.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Gameplay.Powerups
+{
+    public interface IPowerup
+    {
+        /// <summary>
+        /// Modifies the given players attributes in order to apply the effect
+        /// </summary>
+        /// <param name="player">The player that should be affected.</param>
+        /// <returns>coroutine magic</returns>
+        IEnumerator ScheduleEffect(GameObject player);
+        
+        
+        /// <summary>
+        /// Removes all impacts of the effect.
+        /// </summary>
+        void CleanUp();
+    }
+}
---
+++ Assets/Scripts/Gameplay/Powerups/PowerupSoundPlayback.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+namespace Gameplay.Powerups
+{
+    
+    [RequireComponent(typeof(AudioSource))]
+    public class PowerupSoundPlayback : MonoBehaviour
+    {
+        [SerializeField] private AudioClip jumpFx;
+        [SerializeField] private AudioClip speedFx;
+
+        private PowerupTypes Type;
+    
+        #region converter
+        AudioClip GetSoundFromType(PowerupTypes type)
+        {
+            switch (type)
+            {
+                case PowerupTypes.JUMP_BOOST:
+                    return jumpFx;
+                case PowerupTypes.SPEED:
+                    return speedFx;
+            }
+
+            throw new NotImplementedException("There is no sound for this yet lmao");
+        }
+        #endregion
+
+        public void SetType(PowerupTypes aType)
+        {
+            Type = aType;
+        }
+    
+        // Start is called before the first frame update
+        void Start()
+        {
+            AudioClip sfx = GetSoundFromType(Type);
+            var source = GetComponent<AudioSource>();
+            source.clip = sfx;
+            source.Play();
+            StartCoroutine(DestoryAfter(sfx.length));
+        }
+
+        IEnumerator DestoryAfter(float time)
+        {
+            yield return new WaitForSeconds(time);
+            Destroy(gameObject);
+        }
+    }
+}
