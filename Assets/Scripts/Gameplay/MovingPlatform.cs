b64522887ac4f866a0572d24b0fc6080b841b47e
k0ded <59554035+k0ded@users.noreply.github.com>
2021-02-12 10:11:39 +0100

    feat(moving-platforms): Implement moving platforms
    The commit adds a script that moves anything up and down according to a sin wave.

---
+++ Assets/Scripts/Gameplay/MovingPlatform.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+
+    [SerializeField] private float mySpeed;
+    [SerializeField] private float myFrequency;
+ 
+    private Dictionary<Transform, Transform> myPrevParents = new Dictionary<Transform, Transform>();
+    private float myOffsetY;
+    private Transform myTransform;
+    private float myCurrentAmount = 0;
+
+    private void Start()
+    {
+        myOffsetY = transform.position.y;
+        myTransform = GetComponent<Transform>();
+    }
+
+    private void Update()
+    {
+        myCurrentAmount += Time.deltaTime * mySpeed;
+        var yAmount = Mathf.Sin(myCurrentAmount);
+        var y = yAmount * myFrequency * 0.5f;
+        myTransform.position = new Vector3(myTransform.position.x, y + myOffsetY);
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        myPrevParents.Add(other.transform, other.transform.parent);
+        other.transform.SetParent(transform);
+    }
+
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        other.transform.SetParent(myPrevParents[other.transform]);
+        myPrevParents.Remove(other.transform);
+    }
+}
