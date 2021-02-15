76b7444f08f2375144884dd65798ac071347387c
Tristan <tristan@tristanfarkas.dev>
2021-02-15 14:54:48 +0100

    feat(input): add controller support

--- Assets/Scripts/Player/PlayerGroundCheck.cs
+++ Assets/Scripts/Player/PlayerGroundCheck.cs
@@ -11,10 +11,12 @@ public class PlayerGroundCheck : MonoBehaviour
     }
 
     private void OnTriggerEnter2D() {
+        print("enter");
         isTouchingGround = true;
     }
 
     private void OnTriggerExit2D() {
+        print("exit");
         isTouchingGround = false;
     }
 }
