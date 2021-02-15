ae0ba75d494e09dd8823963ad536f72f976355e0
Oles Odynets <olchyk98@gmail.com>
2021-02-15 10:20:39 +0100

    chore(scenes): finish character selection menu

--- Assets/Scripts/UI/ArrowSelect.cs
+++ Assets/Scripts/UI/ArrowSelect.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -13,20 +11,20 @@ public class ArrowSelect : MonoBehaviour
     private CharacterChosen mySpritesHandler;
 
     [SerializeField] GameObject CharacterManager;
-    
-    
+
+
     [SerializeField] private int myPlayerIndex; // at every player the player must input this to identify what number the players are
-    
+
     private int CurrentSpriteIndex = 0; // the current shown character so that the game can display the correct image
-    
+
     // Start is called before the first frame update
-    void Awake()
+    void Start()
     {
         image = GetComponent<Image>();
         mySpritesHandler = CharacterManager.GetComponent<CharacterChosen>();
         mySpritesHandler.PlayerAdd();
     }
-    
+
     public void CharacterChange(int change)
     {
         CurrentSpriteIndex = Mathf.Clamp(CurrentSpriteIndex + change, 0, mySprites.Count -1);
