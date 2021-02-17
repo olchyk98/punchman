350652bd70292847b6bbc606a1a1e9bbf312b084
Oles Odynets <olchyk98@gmail.com>
2021-02-17 10:31:59 +0100

    feat(player): implement universal animations overrider

--- Assets/Scripts/Player/PlayerAnimations.cs
+++ Assets/Scripts/Player/PlayerAnimations.cs
@@ -1,17 +1,42 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace Player
 {
+    [System.Serializable]
+    internal struct TargetedAnimations
+    {
+        [SerializeField] public AnimationClip Kick;
+        [SerializeField] public AnimationClip Punch;
+        [SerializeField] public AnimationClip Jump;
+        [SerializeField] public AnimationClip Move;
+        [SerializeField] public AnimationClip Hit;
+        [SerializeField] public AnimationClip Idle;
+    }
+
     [RequireComponent(typeof(Animator))]
     public class PlayerAnimations : MonoBehaviour
     {
         public Animator Animator { get; private set; } = default;
+        private AnimatorOverrideController myAnimatorOverrider;
+
+        [SerializeField] private TargetedAnimations myAnimations;
 
         private void Start()
         {
             Animator = GetComponent<Animator>();
+            myAnimatorOverrider = new AnimatorOverrideController(Animator.runtimeAnimatorController);
+
+            AssignClips();
+        }
+
+        private void AssignClips()
+        {
+            myAnimatorOverrider["Kick"] = myAnimations.Kick;
+            myAnimatorOverrider["Punch"] = myAnimations.Punch;
+            myAnimatorOverrider["Jump"] = myAnimations.Jump;
+            myAnimatorOverrider["Move"] = myAnimations.Move;
+            myAnimatorOverrider["Hit"] = myAnimations.Hit;
+            myAnimatorOverrider["Idle"] = myAnimations.Idle;
         }
 
         public void PlayAnimation(string label)
@@ -19,5 +44,4 @@ namespace Player
             Animator.SetTrigger(label);
         }
     }
-
 }
