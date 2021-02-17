56c37e3bdf52fdc8af4a0b1cf17c0e705923f503
oles <olchyk98@gmail.com>
2021-02-17 11:10:33 +0100

    fix: move player animations struct to a separate file (#49)

--- Assets/Scripts/Player/PlayerAnimations.cs
+++ Assets/Scripts/Player/PlayerAnimations.cs
@@ -2,17 +2,6 @@
 
 namespace Player
 {
-    [System.Serializable]
-    internal struct TargetedAnimations
-    {
-        [SerializeField] public AnimationClip Kick;
-        [SerializeField] public AnimationClip Punch;
-        [SerializeField] public AnimationClip Jump;
-        [SerializeField] public AnimationClip Move;
-        [SerializeField] public AnimationClip Hit;
-        [SerializeField] public AnimationClip Idle;
-    }
-
     [RequireComponent(typeof(Animator))]
     public class PlayerAnimations : MonoBehaviour
     {
