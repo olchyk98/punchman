075b269b7a0aecbfc28d627ee7a3fe657614ae20
Tristan <tristan@tristanfarkas.dev>
2021-02-18 09:48:35 +0100

    feat(sfx): adds punching sfx (#59)

---
+++ Assets/Scripts/Player/PlayerSoundSource.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace Player
+{
+    [RequireComponent(typeof(AudioSource))]
+    public class PlayerSoundSource: MonoBehaviour
+    {
+        [SerializeField] private AudioClip[] myPunchEffects;
+        
+        private AudioSource myAudioSource;
+
+        private void Start()
+        {
+            myAudioSource = GetComponent<AudioSource>();
+        }
+
+        /// <summary>
+        /// Selects a random punch effect and initiates playback.
+        /// </summary>
+        public void PlayPunchEffect()
+        {
+            int audioIndex = Random.Range(0, myPunchEffects.Length);
+            myAudioSource.clip = myPunchEffects[audioIndex];
+            myAudioSource.Play();
+        }
+
+    }
+}
