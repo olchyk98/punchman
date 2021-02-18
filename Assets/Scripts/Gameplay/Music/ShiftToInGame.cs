e49b50810c688c1cc298ef10624298fd3c1837a0
Tristan <tristan@tristanfarkas.dev>
2021-02-18 20:37:47 +0100

    fix(game): finish scripts to finish the main (#102)

---
+++ Assets/Scripts/Gameplay/Music/ShiftToInGame.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Gameplay.Music;
+using UnityEngine;
+
+public class ShiftToInGame : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void Start()
+    {
+        MusicManager.Main.PlayRandomTrackOfType(MusicType.IN_GAME);
+        StartCoroutine(StartIntenseAfter(60));
+    }
+
+    IEnumerator StartIntenseAfter(int delay)
+    {
+        yield return new WaitForSeconds(delay);
+        MusicManager.Main.PlayRandomTrackOfType(MusicType.IN_GAME_INTENSE);
+    }
+}
---
+++ Assets/Scripts/UI/MainMenuInitializer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Gameplay.Music;
+using UnityEngine;
+
+public class MainMenuInitializer : MonoBehaviour
+{
+
+    private void Start()
+    {
+        StartCoroutine(PlayMusic());
+    }
+
+    private IEnumerator PlayMusic()
+    {
+        yield return 0;
+        MusicManager.Main.PlayRandomTrackOfType(MusicType.MAIN_MENU);
+    }
+}
