c03a6464d4cd168eeff8501c15e9ecd9b8cda336
DaTweaks <51744514+DaTweaks@users.noreply.github.com>
2021-02-18 12:59:55 +0100

    refactor(everything-i-wrote): Add comments

--- Assets/Scripts/UI/CharacterChosen.cs
+++ Assets/Scripts/UI/CharacterChosen.cs
@@ -6,9 +6,9 @@ using UnityEngine;
 public class CharacterChosen : MonoBehaviour
 {
     [SerializeField]
-    List<int> SelectedCharacterIndex = new List<int>();
+    List<int> SelectedCharacterIndex = new List<int>(); // the Selected characters
     [SerializeField]
-    private List<string> characterNames = new List<string>();
+    private List<string> characterNames = new List<string>(); // the playable character names
 
     public void PlayerAdd() // this gets called in the arrowcontroller script and that means that for every character that exists there will be an spot on the list for them
     {
@@ -20,7 +20,7 @@ public class CharacterChosen : MonoBehaviour
         SelectedCharacterIndex[playernr] = character;
     }
 
-    public void ConfirmSelected()
+    public void ConfirmSelected() // when the confirm button gets pressed the GameManager Static class saves the selected characters including the corresponding player id's.
     {
         for (int i = 0; i < 2; i++)
             GameManager.SetCharacter(i, characterNames[SelectedCharacterIndex[i]]);
--- Assets/Scripts/UI/ShowCaseManager.cs
+++ Assets/Scripts/UI/ShowCaseManager.cs
@@ -7,10 +7,9 @@ using UnityEngine;
 public class ShowCaseManager : MonoBehaviour
 {
     [SerializeField]
-    private List<GameObject> Arts = new List<GameObject>();
+    private List<GameObject> Arts = new List<GameObject>(); // the maps that will be previewed
 
-    private int ShownArt = 0;
-    
+    private int ShownArt = 0; // the current shown art that is connected to the Arts List
     
     public void OnHover(int index)
     {
