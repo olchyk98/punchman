9186dd4b4d6e82d39057ec291afcbc0b70f0295a
Oles Odynets <olchyk98@gmail.com>
2021-02-18 16:34:57 +0100

    chore: create splash animations singular handler

---
+++ Assets/Scripts/Gameplay/DestroyAnimatedObject.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class DestroyAnimatedObject : MonoBehaviour
+{
+    private Animator myAnimator;
+
+    private void Start()
+    {
+        myAnimator = GetComponent<Animator>();
+
+        var clipInfo = myAnimator.GetCurrentAnimatorClipInfo(0);
+        StartCoroutine(DestroySelf(clipInfo.Length / 2));
+    }
+
+    private IEnumerator DestroySelf(float time)
+    {
+        yield return new WaitForSeconds(time);
+        DestroyImmediate(gameObject);
+    }
+}
